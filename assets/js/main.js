a7e3cb53df9dc9a1d4a9bcc96e700872ad95e1bb
mitawa0001 <nsmmitawa@gmail.com>
2022-11-11 17:25:20 +0530

    add portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,87 @@
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  /*Active link*/
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
+
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 1000,
+  reset: true,
+});
+
+/*SCROLL HOME*/
+sr.reveal(".home__title", {});
+sr.reveal(".button", { delay: 200 });
+sr.reveal(".home__img", { delay: 400 });
+sr.reveal(".home__social-icon", { interval: 200 });
+
+/*SCROLL ABOUT*/
+sr.reveal(".about__img", {});
+sr.reveal(".about__subtitle", { delay: 400 });
+sr.reveal(".about__text", { delay: 400 });
+
+/*SCROLL SKILLS*/
+sr.reveal(".skills__subtitle", {});
+sr.reveal(".skills__text", {});
+sr.reveal(".skills__data", { interval: 200 });
+sr.reveal(".skills__img", { delay: 600 });
+
+/*SCROLL WORK*/
+sr.reveal(".work__img", { interval: 200 });
+
+/*SCROLL CONTACT*/
+sr.reveal(".contact__input", { interval: 100 });
+
+// form
+const contactForm = document.querySelector(".contact__form");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+
+contactForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const formData = {
+    name: name.value,
+    email: email.value,
+    message: message.value,
+  };
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "/");
+  xhr.setRequestHeader("content-type", "application/json");
+  xhr.onload = function () {
+    if (xhr.responseText == "success") {
+      alert("Thankyou your message has been sent");
+      name = "";
+      email = "";
+      message = "";
+    } else {
+      alert("Sorry something went wrong");
+    }
+  };
+  xhr.send(JSON.stringify(formData));
+});
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const path = require("path");
+var favicon = require("serve-favicon");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const app = express();
+app.use(express.static(path.join(__dirname, "assets")));
+app.use(express.json());
+
+app.use(favicon(path.join(__dirname + "/assets/img/favicon.ico")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+app.post("/", (req, res) => {
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    },
+  });
+  const mailOptions = {
+    from: req.body.email,
+    to: process.env.EMAIL,
+    subject: `Message from ${req.body.name}`,
+    text: req.body.message,
+  }
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send("success");
+    }
+  });
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function () {
+  console.log("Server has started");
+});
